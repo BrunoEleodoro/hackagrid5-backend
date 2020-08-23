0a947bdd1474dd8a68f62a99448b67974636dde9
Bruno Eleodoro Roza <Bruno.Eleodoro.Roza@ibm.com>
2020-08-23 15:26:07 -0300

    docker compose new version

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connection = async function () {
     mongoose.Promise = global.Promise
-    var mongoUrl = process.env.PROD ? process.env.MONGODB : "mongodb://localhost/hackthongr1d"
+    var mongoUrl = process.env.PROD ? String(process.env.MONGODB) : "mongodb://localhost/hackthongr1d"
     try {
         await mongoose.connect(mongoUrl, {
             useNewUrlParser: true,
